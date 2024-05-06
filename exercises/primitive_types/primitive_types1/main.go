97c66160632052e3ac1efb745c5dbe74f1970cc0
sabir222 <mrsabir4@gmail.com>
2024-05-06 02:28:23 +0100

    fix: fixing errors = solutions

--- exercises/primitive_types/primitive_types1/main.go
+++ exercises/primitive_types/primitive_types1/main.go
@@ -1,18 +1,16 @@
 // primitive_types1
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
-	storeIsOpen := true
+	storeIsOpen := false
 	if storeIsOpen {
 		fmt.Println("The store is open, let's buy some clothes!")
 	}
 
-	storeIsOpen
 	if !storeIsOpen {
 		fmt.Println("Oh no, let's buy some clothes online!")
 	}
